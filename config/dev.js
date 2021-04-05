2ca458bd4bd597fd9d9c65c48c2eabd7e7ebeb92
Sam Dawes <sdawes437@gmail.com>
2021-04-05 12:20:02 +0100

    Added caching feature and fixed vulnerabilities

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb://sdawes:M9B1ukOjXq3HN8N1@cluster0-shard-00-00.643e5.mongodb.net:27017,cluster0-shard-00-01.643e5.mongodb.net:27017,cluster0-shard-00-02.643e5.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-nytfhu-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
