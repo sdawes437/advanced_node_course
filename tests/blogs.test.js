71c13a6a10af80369b10a3adb413efb190d17c80
Sam Dawes <sdawes437@gmail.com>
2021-06-02 17:03:22 +0100

    Completed test suite

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -11,11 +11,79 @@ afterEach(async () => {
   await page.close();
 });
 
-test('when logged in, can see blog create form', async () => {
-  await page.login();
-  await page.click('a.btn-floating');
+describe('when logged in', () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+  it('can see blog create form', async () => {
+    await page.login();
+    await page.click('a.btn-floating');
 
-  const label = await page.getContentsOf('form label');
+    const label = await page.getContentsOf('form label');
 
-  expect(label).toEqual('Blog Title');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('and using valid inputs', () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My title');
+      await page.type('.content input', 'My content');
+      await page.click('form button');
+    });
+    it('submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5');
+
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    it('submitting then saving adds blog to index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('My title');
+      expect(content).toEqual('My content');
+    });
+  });
+
+  describe('and using invalid inputs', () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+    it('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+});
+
+describe('user is not logged in', () => {
+  const actions = [
+    {
+      method: 'get',
+      path: '/api/blogs',
+    },
+    {
+      method: 'post',
+      path: '/api/blogs',
+      data: {
+        title: 'My title',
+        content: 'My content',
+      },
+    },
+  ];
+
+  it('blog related actions are prohibited', async () => {
+    const results = await page.execRequests(actions);
+
+    for (let result of results) {
+      expect(result).toEqual({ error: 'You must log in!' });
+    }
+  });
 });
